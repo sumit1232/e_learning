afabbb64341956f1ce7377a0698e88c5cd3c13bb
sumit jadhav <sumitjadhav3710@gmail.com>
2024-04-06 22:08:14 +0530

    database connection

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,45 @@
-import express from "express";
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+
+//body parser
+app.use(express.json({ limit: "50mb" }));
+
+
+
+
+//cookie parser
+app.use(cookieParser());
+
+
+
+
+//cors => cross Origin resource sharing
+app.use(
+    cors({
+        origin: process.env.ORIGIN,
+    })
+);
+
+
+
+
+//Testing AP
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is Working",
+    });
+});
+
+
+//Unknown Route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route  ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+});
